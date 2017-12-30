7cb26e898ee59fa234aafe2f19bd9918b94c543d
sxie22 <sxiexie@ufl.edu>
2017-12-30 17:18:58 -0500

    Complete framework command-line interface for collating, fingerprinting, preprocessing, and network modules with support for reading settings from .ini-like files. Update definition and default values for .ini-like settings.

--- nnf/framework.py
+++ nnf/framework.py
@@ -1,129 +1,114 @@
 """
-This module provides a command-line level argument parser for the framework.
+Command-line level argument parser for the framework.
 
-Representation Workflow:
-1) Read and Collate Structures
-    parse_property: read property values, save to intermediate .hdf5
-    collate_structures: read coordinate and element data,
-                        save to intermediate .hdf5
-2) Fingerprint Structures
-    apply_descriptors: read intermediate .hdf5, process into fingerprints,
-                       write to another .hdf5
+1) Collate Molecules
 
-Neural Network Workflow:
-1) Load Fingerprints from HDF5
-    ...
+2) Make Fingerprints
 
-2) [under construction]
+3) Preprocess Fingerprints
+
+4) Train using Keras model(s)
+
+5) Predict properties (e.g. energy) using trained model(s)
 """
 import os
 import argparse
 import time
+from nnf.batch_collate import BatchCollator
+from nnf.batch_fingerprint import FingerprintProcessor
+from nnf.batch_preprocess import DataPreprocessor
+from nnf.io_utils import SettingsParser
+from nnf.network import Network
 
-from nnf.io_structures import parse_property, collate_structures
-from nnf.io_fingerprints import read_parameters_from_file, apply_descriptors
-from nnf.io_utils import validate_hdf5
-from configparser import ConfigParser
+PACKAGE_PATH = os.path.dirname(__file__)
 
 
 def initialize_argparser():
     """
-
     Initializes ArgumentParser for command-line operation.
-
     """
-    argparser = argparse.ArgumentParser(description='Converts structures to \
-                                        symmetry functions.')
-    argparser.add_argument('action', choices=['fingerprint', 'collate',
-                                              'train','validate'])
-    argparser.add_argument('settings_file',
+    description = 'Framework for fitting neural network potentials.'
+    argparser = argparse.ArgumentParser(description=description)
+    argparser.add_argument('action', choices=['Collate', 'Fingerprint',
+                                              'Preprocess', 'Network',
+                                              'GridSearch'])
+    argparser.add_argument('--settings_file', '-s', default='settings.cfg',
                            help='Filename of settings.')
-    argparser.add_argument('--verbose', '-v', action='count')
-    return argparser
-
-
-class Settings:
-    def __init__(self, action):
-        self.definition_parser = ConfigParser()
-        self.definition_parser.read("definitions.cfg")
-        self.types = {}
-        for option, value in self.definition_parser.items(action):
-            self.types.setdefault(value, []).append(option)
-        self.parser = ConfigParser()
-        self.section = self.parser[action]
-        self.settings = {}
-        self.read('defaults.cfg')
-
-    def read(self, filename):
-        self.parser.read(filename)
-        for key in self.types.get('str', []):
-            self.settings[key] = self.section.get(key)
-        for key in self.types.get('int', []):
-            self.settings[key] = self.section.getint(key)
-        for key in self.types.get('float', []):
-            self.settings[key] = self.section.getfloat(key)
-        for key in self.types.get('bool', []):
-            self.settings[key] = self.section.getboolean(key)
-        for key in self.types.get('strs', []):
-            self.settings[key] = self.section.get(key).split(',')
-        for key in self.types.get('ints', []):
-            self.settings[key] = [int(val)
-                                  for val in self.section.get(key).split(',')]
-        for key in self.types.get('floats', []):
-            self.settings[key] = [float(val)
-                                  for val in self.section.get(key).split(',')]
-        for key in self.types.get('bools', []):
-            self.settings[key] = [bool(val)
-                                  for val in self.section.get(key).split(',')]
-        return self.settings
+    argparser.add_argument('--verbosity', '-v', default=0,
+                           action='count')
+    argparser.add_argument('--export', '-E', action='store_true',
+                           help='Export entries to csv.')
+    argparser.add_argument('--savefigs', '-f', action='store_true',
+                           help='Save figures.')
+    args = argparser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
-    args = initialize_argparser().parse_args()
+    args = initialize_argparser()
     action = args.action
-    settings_file = args.settings_file
-    verbosity = args.verbosity
+    settings = SettingsParser(action).read(args.settings_file)
+    settings['verbosity'] = args.verbosity
 
-    settings = Settings(action).read(settings_file)
-    input_name = settings['inputs_file']
-    index = settings['index']
-    output_name = settings['outputs_file']
-    sys_elements = settings['sys_elements']
-
-    if not len(sys_elements) == len(set(sys_elements)):
-        raise ValueError('duplicate elements: ' + sys_elements)
-
-    if ':' not in settings['index']:
-        raise ValueError('invalid slice specified: ' + args.index)
-
-    global t0
+    inputs_name = settings['inputs_name']
+    outputs_name = settings['outputs_name']
     t0 = time.time()
 
-    if args.action == 'fingerprint':
-        parameters = read_parameters_from_file(settings['parameters_file'])
-        descriptor = settings['descriptor']
-        derivs = settings['derivatives']
-        apply_descriptors(input_name, output_name, sys_elements, parameters,
-                          index=index, descriptor=descriptor,
-                          derivs=derivs)
-        validate_hdf5(output_name, sys_elements=sys_elements)
-
-    elif args.action == 'collate':
-        loose = os.path.isdir(input_name)
+    if action == 'Collate':
         energies_file = settings['energies_file']
-        form = settings['input_format']
-        keyword = settings['keyword']
-        with open(energies_file, 'r') as propfile:
-            proptext = propfile.read()
-        energy_data = parse_property(proptext, loose=loose,
-                                     keyword=keyword,
-                                     index=index)
-        # dictionary if loose, list otherwise
-        collate_structures(input_name, output_name, sys_elements,
-                           energy_data, index=index, form=form)
-        validate_hdf5(output_name, sys_elements=sys_elements)
-
-    elif args.action == 'validate':
-        validate_hdf5(input_name, sys_elements=sys_elements)
-
-    print(time.time() - t0, 'seconds elapsed.')
+        sys_elements = settings['sys_elements']
+        assert sys_elements != ['None']
+        assert len(sys_elements) == len(set(sys_elements))
+        assert ':' in settings['index']
+        collator = BatchCollator(outputs_name, settings, sys_elements)
+        if os.path.isfile(inputs_name):
+            collator.parse_molecules(inputs_name, energies_file)
+        else:
+            collator.parse_loose_molecules(inputs_name, energies_file)
+        if args.export:
+            collator.export_entries(inputs_name.split('.')[0] + '.csv')
+    elif action == 'Fingerprint':
+        parameters_file = settings['parameters_file']
+        sys_elements = settings['sys_elements']
+        assert sys_elements != ['None']
+        assert len(sys_elements) == len(set(sys_elements))
+        assert ':' in settings['index']
+        processor = FingerprintProcessor(outputs_name, parameters_file,
+                                         settings)
+        processor.process_collated(inputs_name)
+        if args.export:
+            processor.export_entries(inputs_name.split('.')[0] + '.csv')
+    elif action == 'Preprocess':
+        partitions_file = settings['partitions_file']
+        split_fraction = settings['split_fraction']
+        kfold = settings['kfold']
+        simple = settings['simple']
+        sys_elements = settings['sys_elements']
+        assert sys_elements != ['None']
+        assert len(sys_elements) == len(set(sys_elements))
+        assert ':' in settings['index']
+        preprocessor = DataPreprocessor(settings)
+        preprocessor.read_fingerprints(inputs_name)
+        subdivisions = [int(val) for val in settings['subdivisions']]
+        preprocessor.subdivide_by_parameter_set(subdivisions)
+        preprocessor.preprocess_fingerprints()
+        preprocessor.to_file(outputs_name)
+        if simple:
+            preprocessor.generate_simple_partitions(partitions_file,
+                                                    split_fraction,
+                                                    k=kfold)
+        else:
+            preprocessor.generate_partitions(partitions_file, split_fraction,
+                                             k=kfold)
+    elif action == 'Network':
+        partitions_file = settings['partitions_file']
+        network = Network(settings)
+        network.load_data(inputs_name, partitions_file, settings)
+        final_loss = network.train_network(settings)
+        print('\n\nFinal loss:', final_loss)
+    elif action == 'GridSearch':
+        network_settings = SettingsParser('Network').read(args.settings_file)
+        network_settings['verbosity'] = args.verbosity
+        network = Network(network_settings)
+        network.grid_search(args.settings_file, outputs_name)
+    print('\n\n{}'.format(time.time() - t0), 'seconds elapsed.')
