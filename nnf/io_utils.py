a9ed21e5d2d35e903086c5e2f195bfdb9ad7bd1c
sxie22 <sxiexie@ufl.edu>
2017-12-30 17:24:24 -0500

    Move SettingsParser from framework.py to address issue with importing. Minor bug fixes.

--- nnf/io_utils.py
+++ nnf/io_utils.py
@@ -1,14 +1,19 @@
 """
 
 """
+import os
 import string
+from configparser import ConfigParser
+
 import numpy as np
+
+PACKAGE_PATH = os.path.dirname(__file__)
 
 ALPHABET = (string.ascii_uppercase + string.ascii_lowercase
             + string.digits)
 ALPHABET_REVERSE = dict((c, i) for (i, c) in enumerate(ALPHABET))
 BASE = len(ALPHABET)
-SIGN_CHARACTER = '$'
+SIGN_CHARACTER = '0'
 
 
 def num_encode(n):
@@ -32,8 +37,9 @@ def num_decode(s):
 
 
 def generate_tag(unique_dict, add_on='0'):
-    j_ints = ''.join([str(val) for val
-                      in unique_dict.values()
+    j_ints = ''.join([str(val) if val == abs(val)
+                      else '0{}'.format(abs(val))
+                      for val in unique_dict.values()
                       if (isinstance(val, int)
                           and not (isinstance(val, bool)))])
     floats = [np.log(val) * 10
@@ -50,7 +56,8 @@ def generate_tag(unique_dict, add_on='0'):
 
 
 def grid_string(entries, max_length=79, separator=', ', newline=';\n'):
-    assert len(entries) > 0
+    if len(entries) == 0:
+        return ''
     max_width = max([len(entry) for entry in entries])
     grid_lines = ''
     grid_line = str(entries.pop(0)).ljust(max_width)
@@ -61,6 +68,7 @@ def grid_string(entries, max_length=79, separator=', ', newline=';\n'):
             grid_line = new_string
         else:
             grid_line += separator + new_string
+    grid_lines += grid_line + newline
     return grid_lines
 
 
@@ -135,3 +143,47 @@ def write_to_group(h5f, group_path, dict_attrs, dict_dsets,
 
     for attr_name, attr_data in dict_attrs.items():
         group.attrs[attr_name] = attr_data
+
+
+class SettingsParser:
+    """
+    SettingsParser parser.
+
+    Args:
+        action (str): Config key from which to read settings.
+    """
+
+    def __init__(self, action):
+        self.action = action
+        self.settings = {}
+        self.types = {}
+        self.definitions = ConfigParser()
+        self.definitions.read(os.path.join(PACKAGE_PATH, 'definitions.cfg'))
+        for option, value in self.definitions.items(action):
+            self.types.setdefault(value, []).append(option)
+        self.parser = ConfigParser()
+        self.read(os.path.join(PACKAGE_PATH, 'defaults.cfg'))
+
+    def read(self, filename):
+        self.parser.read(filename)
+        self.section = self.parser[self.action]
+        for key in self.types.get('str', []):
+            self.settings[key] = self.section.get(key)
+        for key in self.types.get('int', []):
+            self.settings[key] = self.section.getint(key)
+        for key in self.types.get('float', []):
+            self.settings[key] = self.section.getfloat(key)
+        for key in self.types.get('bool', []):
+            self.settings[key] = self.section.getboolean(key)
+        for key in self.types.get('strs', []):
+            self.settings[key] = self.section.get(key).split(',')
+        for key in self.types.get('ints', []):
+            self.settings[key] = [int(val)
+                                  for val in self.section.get(key).split(',')]
+        for key in self.types.get('floats', []):
+            self.settings[key] = [float(val)
+                                  for val in self.section.get(key).split(',')]
+        for key in self.types.get('bools', []):
+            self.settings[key] = [bool(val)
+                                  for val in self.section.get(key).split(',')]
+        return self.settings
